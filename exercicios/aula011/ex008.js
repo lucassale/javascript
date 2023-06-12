11ec9958e4164ba5c0ef5eb3f03a2d0dd076dec5
Carlos Lucas Sales de Andrade <132520068+lucassale@users.noreply.github.com>
2023-06-12 14:53:30 -0300

    atualizado até o exercicio011

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 90
+console.log(`Sua velocidade é de ${vel}`)
+if (vel > 80){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+else{
+    console.log('Dirija sempre com segurança!')
+}
---
+++ exercicios/aula011/ex009.js
@@ -0,0 +1,8 @@
+var p = 'Portugal'
+console.log(`Você mora no(a) ${p}.`)
+if (p == 'Brasil'){
+    console.log('Você é brasileiro!')
+}
+else{
+    console.log('Você é estrangeiro!')
+}
