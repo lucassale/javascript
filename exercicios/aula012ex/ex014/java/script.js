7a97518efc18acf23f7c6a87fcc899c4766b8a08
Carlos Lucas Sales de Andrade <132520068+lucassale@users.noreply.github.com>
2023-06-14 16:07:28 -0300

    ex012 finalizado

--- exercicios/aula012ex/ex014/java/script.js
+++ exercicios/aula012ex/ex014/java/script.js
@@ -1,1 +1,17 @@
-alert.main('Olá!')
+function carregar(){
+    var m = document.getElementById('msg')
+    var i = document.getElementById('imagem')
+    var h = new Date()
+    var hora = h.getHours()
+    m.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        i.src = 'imagens/dia.png'
+        document.body.style.background = '#f1d7be'
+    }else if(hora >= 12 && hora < 18){
+        i.src = 'imagens/tarde.png'
+        document.body.style.background = '#cc8251'
+    }else{
+        i.src = 'imagens/noite.png'
+        document.body.style.background = '#42365a'
+    }
+}
