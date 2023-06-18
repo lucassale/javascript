19fc56fd3ab6ecf3dcb9828564cf243824bcc651
Carlos Lucas Sales de Andrade <132520068+lucassale@users.noreply.github.com>
2023-06-17 21:03:34 -0300

    Finalizado ex017

--- exercicios/aula14ex/ex017/modelo/java/script.js
+++ exercicios/aula14ex/ex017/modelo/java/script.js
@@ -1,1 +1,18 @@
-alert.main('Olá!')
+function tabuada(){
+    var v = document.getElementById('num')
+    var s = document.getElementById('tab')
+    if(v.value.length == 0){
+        alert ('Não encontrei nenhum número...')
+    }else{
+        var n = Number(v.value)
+        var c = 1
+        s.innerHTML = ''
+        while(c <= 10){
+            var item = document.createElement('option')
+            item.text = (`${n} x ${c} = ${n*c}`)
+            s.appendChild (item)
+            c++
+        }
+
+        }
+    }
